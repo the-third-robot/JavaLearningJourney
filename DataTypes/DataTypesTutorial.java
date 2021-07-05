c68a1b7c1a9213a99ee2d1960820ecddeb35aec0
Rushikesh Prakash Kumbhar <51785772+the-third-robot@users.noreply.github.com>
2021-07-06 03:29:27 +0530

    All DataTypes Included

---
+++ DataTypes/DataTypesTutorial.java
@@ -0,0 +1,23 @@
+package dataTypes;
+
+public class DataTypesTutorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		byte x = 2;
+		
+		int age = 10;
+		
+		float rateOfInterest = 1.8f;
+		
+		double rate = 3.564444;
+		
+		System.out.println(x);
+		
+		boolean isThisSeriesCool = true;
+		
+		char myName = 'C';		
+	}
+
+}
